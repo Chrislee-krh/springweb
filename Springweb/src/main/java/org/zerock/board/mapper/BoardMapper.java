3e74fec3290df58bb4f01c1f2caeca76834bbd76
tjoeun <tjoeun@DESKTOP-N876NQ2.mshome.net>
2023-10-25 17:44:05 +0900

    2023-10-25

---
+++ Springweb/src/main/java/org/zerock/board/mapper/BoardMapper.java
@@ -0,0 +1,24 @@
+package org.zerock.board.mapper;
+
+import java.util.List;
+
+import org.zerock.board.command.BoardVO;
+
+public interface BoardMapper {
+
+	//게시판 등록
+	public void insertBoard(BoardVO vo);
+	
+	//게시판 수정
+	public boolean updateBoard(BoardVO vo);
+	
+	//게시판 삭제
+	public void deleteBoard(String num);
+	
+	//게시판 목록 가져오기
+	public List<BoardVO> getList();
+	
+	//게시판 글 상세 보기
+	public BoardVO getContent(String num); 
+	
+}
